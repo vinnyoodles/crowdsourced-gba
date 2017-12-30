79e68ea47eca89e0556652c4b5f5e1b4c2c6d6e3
Vincent Le <vinnyoodles@gmail.com>
2017-12-30 02:01:41 -0500

    Create simple python server

--- src/server.py
+++ src/server.py
@@ -1,4 +1,15 @@
+import os.path
 from tornado import websocket, web, ioloop
 
-print('Hello')
+class IndexHandler(web.RequestHandler):
+    def get(self):
+        self.write('Hello World!')
+
+server = web.Application(
+    [(r'/', IndexHandler), (r'/favicon.ico', web.StaticFileHandler, {'path': 'static'})]
+)
+
+if __name__ == '__main__':
+    server.listen(3000)
+    ioloop.IOLoop.instance().start()
 
