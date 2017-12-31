0ae36bb6ac4e2d839ce5bda970bb3d9d3f3d0113
Puyush Goel <puyushgoel@gmail.com>
2017-12-30 21:49:56 -0800

    added the python server to communicate with the webpage

--- src/server/server.py
+++ src/server/server.py
@@ -1,14 +1,35 @@
 import os.path
 from tornado import websocket, web, ioloop
+import socket
 
-class IndexHandler(web.RequestHandler):
+class Indexhandler(web.RequestHandler):
     def get(self):
-        self.write('Hello World!')
+        self.render("index.html")
 
-def run():
-    server = web.Application([
-        (r'/', IndexHandler), 
-        (r'/favicon.ico', web.StaticFileHandler, {'path': 'static'})
-    ])
-    server.listen(3000)
+class SocketHandler(websocket.WebSocketHandler):
+    def open(self):
+        print 'new connection'
+
+    def on_message(self, message):
+        print 'message received: %s' % message
+
+        print 'send the message back: %s' % message[::]
+        self.write_message(message[::])
+
+    def on_close(self):
+        print 'connection closed'
+
+    def check_origin(self, orgin):
+        return True
+
+app = web.Application([
+    (r'/', Indexhandler),
+    (r'/ws', SocketHandler),
+    (r'/(favicon.ico)', web.StaticFileHandler, {'path': '../'}),
+])
+
+if __name__ == '__main__':
+    app.listen(8888)
+    myIP = socket.gethostbyname(socket.gethostname())
+    print '*** Websocket Server Started at %s***' % myIP
     ioloop.IOLoop.instance().start()
