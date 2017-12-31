dc7c0ecef843c7e680ec3f4844da63ae6ce8d9df
Vincent Le <vinnyoodles@gmail.com>
2017-12-30 21:23:17 -0500

    Fix configuring emulator core

--- src/server/emulator.py
+++ src/server/emulator.py
@@ -1,17 +1,14 @@
-from mgba.core import Core, find
+import mgba.core, mgba.image
 
 def run(rom_path):
-    core = find(rom_path)
+    core = mgba.core.loadPath(rom_path)
     width, height = core.desiredVideoDimensions()
-    core.setVideoBuffer(width)
-    if not core.loadFile(rom_path):
-        print('Failed to load rom file')
-        return
-
-    core.loadCoreConfig()
+    image = mgba.image.Image(width, height)
+    core.setVideoBuffer(image)
 
     # Reset the core. This is needed before it can run.
     core.reset()
 
-    # while True:
-    #     core.runFrame()
+    while True:
+        core.runFrame()
+        print(image.toPIL())
