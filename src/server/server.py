f58efd68d5ac4104d1b726e75bdf7e0b2d2c71e0
Puyush Goel <puyushgoel@gmail.com>
2017-12-30 22:09:39 -0800

    clean print statments and remove styling from html

--- src/server/server.py
+++ src/server/server.py
@@ -7,28 +7,23 @@ class IndexHandler(web.RequestHandler):
 
 class SocketHandler(websocket.WebSocketHandler):
     def open(self):
-        print 'new connection'
+        pass
 
     def on_message(self, message):
-        print 'message received: %s' % message
-
-        print 'send the message back: %s' % message[::]
         self.write_message(message[::])
 
     def on_close(self):
-        print 'connection closed'
+        pass
 
     def check_origin(self, orgin):
         return True
 
 app = web.Application([
-    (r'/', Indexhandler),
+    (r'/', IndexHandler),
     (r'/ws', SocketHandler),
     (r'/(favicon.ico)', web.StaticFileHandler, {'path': '../'}),
 ])
 
 if __name__ == '__main__':
     app.listen(8888)
-    myIP = socket.gethostbyname(socket.gethostname())
-    print '*** Websocket Server Started at %s***' % myIP
     ioloop.IOLoop.instance().start()
