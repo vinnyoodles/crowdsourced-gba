6dab57c6940e610dd8f66c81ca599667d361f886
Puyush Goel <puyushgoel@gmail.com>
2018-01-03 18:14:28 -0800

    Added a setup function to ensure the test don't rely on prior states #11

--- src/test_ringbuffer.py
+++ src/test_ringbuffer.py
@@ -2,25 +2,31 @@ import unittest
 import ringbuffer
 
 class TestRingBuffer(unittest.TestCase):
-    buf = ringbuffer.RingBuffer(3)
+
     def setUp(self):
-        pass
+        self.ringbuffer = ringbuffer.RingBuffer(3)
+
+    def tearDown(self):
+        del self.ringbuffer
 
     def test_append(self):
-        TestRingBuffer.buf.append(1)
-        self.assertEquals(len(TestRingBuffer.buf._data), 1)
-        TestRingBuffer.buf.append(2)
-        self.assertEquals(len(TestRingBuffer.buf._data), 2)
-        TestRingBuffer.buf.append(3)
-        self.assertEquals(len(TestRingBuffer.buf._data), 3)
+        self.ringbuffer.append(1)
+        self.assertEquals(len(self.ringbuffer._data), 1)
+        self.ringbuffer.append(2)
+        self.assertEquals(len(self.ringbuffer._data), 2)
+        self.ringbuffer.append(3)
+        self.assertEquals(len(self.ringbuffer._data), 3)
 
     def test_get_k_recent(self):
-        self.assertEquals(TestRingBuffer.buf.get_k_recent(3), [3, 2, 1])
-        TestRingBuffer.buf.append(100)
-        self.assertEquals(TestRingBuffer.buf.get_k_recent(4), [100, 3 , 2])
+        self.ringbuffer.append(1)
+        self.ringbuffer.append(2)
+        self.ringbuffer.append(3)
+        self.assertEquals(self.ringbuffer.get_k_recent(3), [3, 2, 1])
+        self.ringbuffer.append(100)
+        self.assertEquals(self.ringbuffer.get_k_recent(4), [100, 3 , 2])
 
         with self.assertRaises(Exception) as context:
-            TestRingBuffer.buf.get_k_recent(-1)
+            self.ringbuffer.get_k_recent(-1)
         self.assertTrue('Invalid Lookup' in context.exception)
 
 if __name__ == '__main__':
