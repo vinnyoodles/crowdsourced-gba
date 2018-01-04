5fb93e2e00962dbfaa96722a6e6db753a6b8bf1f
Puyush Goel <puyushgoel@gmail.com>
2018-01-03 16:08:44 -0800

    ringbuffer returns a list for k recent elements #10

--- src/ringbuffer.py
+++ src/ringbuffer.py
@@ -3,20 +3,30 @@ class RingBuffer:
         self.index = 0
         self.size = size
         self._data = []
+        self.last_elem = 0
+        self.first_elem = 0
 
     def append(self, value):
         if len(self._data) == self.size:
+            self.first_elem = (self.first_elem + 1) % self.size
             self._data[self.index] = value
         else:
             self._data.append(value)
+        self.last_elem = self.index
         self.index = (self.index + 1) % self.size
 
+
     def get_k_recent(self, key):
-        if key > 1:
-            if len(self._data) == self.size:
-                # starting index + k recent
-                return self._data[(key - 1) % self.size]
-            else:
-                return self._data[key]
+        k_recents = list()
+        count = self.last_elem
+        if key <= 0:
+            raise LookupError("Invalid Lookup")
         else:
-            return 'Invalid Lookup'
+            while key != 0 and count != self.first_elem:
+                k_recents.append(self._data[count])
+                count -= 1
+                if count < 0:
+                    count = self.size - 1
+                key -= 1
+            k_recents.append(self._data[count])
+            return k_recents
