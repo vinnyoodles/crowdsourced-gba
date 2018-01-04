0a0e97b6f6391ae0a4ff7623561f07957644d03d
Puyush Goel <puyushgoel@gmail.com>
2018-01-03 16:11:22 -0800

    Test updated for get_k_recent function

--- src/test_ringbuffer.py
+++ src/test_ringbuffer.py
@@ -15,11 +15,13 @@ class TestRingBuffer(unittest.TestCase):
         self.assertEquals(len(TestRingBuffer.buf._data), 3)
 
     def test_get_k_recent(self):
-        self.assertEquals(TestRingBuffer.buf.get_k_recent(3), 3)
+        self.assertEquals(TestRingBuffer.buf.get_k_recent(3), [3, 2, 1])
         TestRingBuffer.buf.append(100)
-        self.assertEquals(TestRingBuffer.buf.get_k_recent(4), 100)
-        self.assertEquals(TestRingBuffer.buf.get_k_recent(0), 'Invalid Lookup')
+        self.assertEquals(TestRingBuffer.buf.get_k_recent(4), [100, 3 , 2])
 
+        with self.assertRaises(Exception) as context:
+            TestRingBuffer.buf.get_k_recent(-1)
+        self.assertTrue('Invalid Lookup' in context.exception)
 
 if __name__ == '__main__':
     unittest.main()
